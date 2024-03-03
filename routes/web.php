f95511e11ddd8df309a03f31d5990b3e2c9822bc
khalid oukha <oukhakhalid@gmail.com>
2024-03-03 17:40:44 +0100

    finished hero section and about us

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\StatisticsController;
+use App\Http\Controllers\backoffice\CoachController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Home Routes...
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication Routes...
+
+Route::get('/login', [LoginController::class,'showLoginForm'])->name('loginform');
+Route::post('/login', [LoginController::class,'login'])->name('login');
+Route::post('/logout', [LoginController::class,'logout'])->name('logout');
+
+// Registration Routes...
+Route::get('/register', [RegisterController::class,'showRegistrationForm'])->name('registerform');
+Route::post('/register', [RegisterController::class,'register'])->name('register');
+
+// Password Reset Routes...
+Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('forget.passwordform');
+Route::post('/password/email', [ForgotPasswordController::class, 'ResetPasswordRequest'])->name('forgetpassword.request');
+Route::get('reset/{token}', [ResetPasswordController::class,'reset'])->name('password.reset');
+Route::post('/reset', [ResetPasswordController::class,'GetnewPassword'])->name('password.postReset');
+
+
+// dashboard Routes...
+
+
+Route::prefix('admin')->middleware(['is_admin'])->group(function () {
+        Route::get('/', [StatisticsController::class, 'index'])->name('index.statistics');
+        // Coash Routes...
+
+        Route::get('/coachs',[CoachController::class, 'index'])->name('coash.index');
+
+});
+    
