c077d5ca545539586b0d5bd89af2760e415c0508
khalid oukha <oukhakhalid@gmail.com>
2024-03-08 11:38:12 +0100

    add middlewares to protect endpoints

--- routes/web.php
+++ routes/web.php
@@ -40,58 +40,76 @@ Route::get('/', function () {
 
 // Authentication Routes...
 
-Route::get('/login', [LoginController::class,'showLoginForm'])->name('loginform');
-Route::post('/login', [LoginController::class,'login'])->name('login');
-Route::post('/logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('loginform');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Registration Routes...
-Route::get('/register', [RegisterController::class,'showRegistrationForm'])->name('registerform');
-Route::post('/register', [RegisterController::class,'register'])->name('register');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('registerform');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
 
-// infos for organizer Routes...
-Route::get('/organizer/form/{userId}', [OrganizerController::class, 'showOrganizerForm'])->name('organizer.form');
-Route::post('/organizer/store', [OrganizerController::class, 'storeOrganizerInfo'])->name('organizer.store');
+
 
 // Password Reset Routes...
 Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('forget.passwordform');
 Route::post('/password/email', [ForgotPasswordController::class, 'ResetPasswordRequest'])->name('forgetpassword.request');
-Route::get('reset/{token}', [ResetPasswordController::class,'reset'])->name('password.reset');
-Route::post('/reset', [ResetPasswordController::class,'GetnewPassword'])->name('password.postReset');
+Route::get('reset/{token}', [ResetPasswordController::class, 'reset'])->name('password.reset');
+Route::post('/reset', [ResetPasswordController::class, 'GetnewPassword'])->name('password.postReset');
 
 
 // dashboard Routes...
 
 
 
-// category Routes...
-Route::resource('category',CategoryController::class);
 
-// Event Routes...
-Route::resource('event',EventController::class);
 
-Route::get('/events/{eventId}/activate', [EventController::class, 'activateEvent'])->name('events.activate');
-Route::get('/events/{eventId}/cancel', [EventController::class, 'cancelEvent'])->name('events.cancel');
+
 
 
 // home Routes...
-Route::get('/', [UpcomingEventsController::class,'index'])->name('home');
-Route::get('/event/{event}/show', [EventController::class,'ShowEvent'])->name('event.details');
+Route::get('/', [UpcomingEventsController::class, 'index'])->name('home');
+Route::get('/event/{event}/show', [EventController::class, 'ShowEvent'])->name('event.details');
 
 // all events Routes...
-Route::get('/events', [EventController::class,'fetchEvents'])->name('event.all');
+Route::get('/events', [EventController::class, 'fetchEvents'])->name('event.all');
 
-// profile Routes...
-Route::get('/profile', [OrganizerProfile::class,'index'])->name('profile.index');
-Route::get('create/organizerevent', [OrganizerProfile::class,'create'])->name('create.event');
-Route::post('store/event', [OrganizerProfile::class,'store'])->name('store.event');
 
-// Reservation Routes ...
-Route::get('/booking/{event}/event', [ReservationsController::class,'bookEvent'])->name('booking.event');
 
-Route::get('/organizer/{event}/reservations', [EventReservationController::class,'index'])->name('orangizer.reservations');
-Route::get('/reservations/{reservation}/activate', [EventReservationController::class, 'activateReservation'])->name('reservation.activate');
-Route::get('/reservations/{reservations}/cancel', [EventReservationController::class, 'cancelReservation'])->name('reservation.cancel');
 
 
 Route::post('/searchBycategorie', [StoreFiltersController::class, 'filterByCategorie'])->name('searchBycategorie');
-Route::get('/search',[SearchController::class,'search'])->name('search');
+Route::get('/search', [SearchController::class, 'search'])->name('search');
+
+
+Route::middleware(['auth', 'organizer'])->group(function () {
+
+    // infos for organizer Routes...
+    Route::get('/organizer/form/{userId}', [OrganizerController::class, 'showOrganizerForm'])->name('organizer.form');
+    Route::post('/organizer/store', [OrganizerController::class, 'storeOrganizerInfo'])->name('organizer.store');
+
+    Route::get('/organizer/{event}/reservations', [EventReservationController::class, 'index'])->name('orangizer.reservations');
+    Route::get('/reservations/{reservation}/activate', [EventReservationController::class, 'activateReservation'])->name('reservation.activate');
+    Route::get('/reservations/{reservations}/cancel', [EventReservationController::class, 'cancelReservation'])->name('reservation.cancel');
+
+    // profile Routes...
+    Route::get('/profile', [OrganizerProfile::class, 'index'])->name('profile.index');
+    Route::get('create/organizerevent', [OrganizerProfile::class, 'create'])->name('create.event');
+    Route::post('store/event', [OrganizerProfile::class, 'store'])->name('store.event');
+});
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    // category Routes...
+    Route::resource('category', CategoryController::class);
+
+    // Event Routes...
+    Route::resource('event', EventController::class);
+
+    Route::get('/events/{eventId}/activate', [EventController::class, 'activateEvent'])->name('events.activate');
+    Route::get('/events/{eventId}/cancel', [EventController::class, 'cancelEvent'])->name('events.cancel');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/booking/{event}/event', [ReservationsController::class, 'bookEvent'])->name('booking.event');
+});
